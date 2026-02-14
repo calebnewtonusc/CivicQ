2fc76b63ed76657bef013e013ec71f44fbb2893f
calebnewtonusc <calebnew@usc.edu>
2026-02-14 04:18:44 -0800

    Remove unused AuthProvider import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { AuthProvider } from './contexts/AuthContext';
 
 // Pages
 import SimpleHomePage from './pages/SimpleHomePage';
